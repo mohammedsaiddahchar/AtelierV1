a41cacce3f7d7aebc7a93debb7da0c7a540fbbe9
mohammedsaiddahchar <65324937+mohammedsaiddahchar@users.noreply.github.com>
2020-05-14 03:28:20 +0000

    Add files via upload

--- Modele.php
+++ Modele.php
@@ -1,6 +1,10 @@
 <?php 
 function getEleves(){
-$bdd = new PDO('mysql:host=localhost;dbname=ensat;charset=utf8', 'root', '');
+$bdd = getBdd();
        $eleves = $bdd->query('select CNE,Nom,Prenom,Adresse,Ville,email, Photo, etat from eleves');
        return $eleves;
+}
+function getBdd(){
+	$bdd = new PDO('mysql:host=localhost;dbname=ensat;charset=utf8', 'root', '');
+	return $Bdd;
 }
